f570299d42e9adb58ee6744f6ac1fed67997e9b6
Joseph Abrahamson <me@jspha.com>
2013-06-08 11:51:11 -0400

    * src/Parse.hs: Cleaned up the alignment

--- src/Parse.hs
+++ src/Parse.hs
@@ -25,17 +25,20 @@ parseExp :: String -> Maybe Exp
 parseExp = fmap Exp . hush . parse (expr <* eof) "expression"
 
 L.TokenParser {
+  
   L.parens         = parens,
   L.reservedOp     = reservedOp,
   L.reserved       = reserved,
   L.whiteSpace     = whiteSpace,
   L.naturalOrFloat = naturalOrFloat,
   L.stringLiteral  = stringLiteral
-  } = L.makeTokenParser
-      emptyDef { L.identLetter     = alphaNum
-               , L.identStart      = letter
-               , L.reservedNames   = words "true false",
-                 L.reservedOpNames = words "& | ! - + * > < >= <= == ~" }
+
+  } =
+  
+  L.makeTokenParser emptyDef { L.identLetter     = alphaNum
+                             , L.identStart      = letter
+                             , L.reservedNames   = words "true false"
+                             , L.reservedOpNames = words "& | ! - + * > < >= <= == ~" }
 
 hush :: Either s a -> Maybe a
 hush (Left s) = Nothing
@@ -51,18 +54,18 @@ term  =      parens expr
 
 expr  =  buildExpressionParser table term <?> "expression"
 
-table =  [ [ prefix "-" (Monop OpNegate . Exp), prefix "+" id ]
-         , [ binary "*" (Binop OpMult `on` Exp) AssocLeft ]
-         , [ binary "+" (Binop OpPlus `on` Exp) AssocLeft,
-             binary "-" (Binop OpMinus `on` Exp) AssocLeft ]
-         , [ binary "<" (Binop OpLT `on` Exp) AssocLeft,
-             binary ">" (Binop OpGT `on` Exp) AssocLeft,
-             binary "<=" (Binop OpLTE `on` Exp) AssocLeft,
-             binary ">=" (Binop OpGTE `on` Exp) AssocLeft,
-             binary "==" (Binop OpApprox `on` Exp) AssocLeft,
-             binary "~" (Binop OpApprox `on` Exp) AssocLeft ]
-         , [ binary "&" (Binop OpAnd `on` Exp) AssocLeft,
-             binary "|" (Binop OpOr `on` Exp) AssocLeft ] ]
+table =  [ [ prefix "-"  (Monop OpNegate . Exp), prefix "+" id ]
+         , [ binary "*"  (Binop OpMult `on` Exp)    AssocLeft ]
+         , [ binary "+"  (Binop OpPlus `on` Exp)    AssocLeft,
+             binary "-"  (Binop OpMinus `on` Exp)   AssocLeft ]
+         , [ binary "<"  (Binop OpLT `on` Exp)      AssocLeft,
+             binary ">"  (Binop OpGT `on` Exp)      AssocLeft,
+             binary "<=" (Binop OpLTE `on` Exp)     AssocLeft,
+             binary ">=" (Binop OpGTE `on` Exp)     AssocLeft,
+             binary "==" (Binop OpApprox `on` Exp)  AssocLeft,
+             binary "~"  (Binop OpApprox `on` Exp)  AssocLeft ]
+         , [ binary "&"  (Binop OpAnd `on` Exp)     AssocLeft,
+             binary "|"  (Binop OpOr `on` Exp)      AssocLeft ] ]
 
 
 val   =      Val (Bool True)  <$  reserved "true"      
@@ -73,22 +76,3 @@ val   =      Val (Bool True)  <$  reserved "true"
   where allFloat = f <$> naturalOrFloat
         f (Left  int)   = fromIntegral int
         f (Right float) = float
-
-
--- monop =     OpNot    <$ string "not"
---         <|> OpNegate <$ string "neg"
-
--- binop =     OpAnd    <$ string "&&"
---         <|> OpOr     <$ string "||"
---         <|> OpLTE    <$ string "<="
---         <|> OpGTE    <$ string ">="
---         <|> OpLT     <$ string "<"
---         <|> OpGT     <$ string ">"
---         <|> OpApprox <$ string "="
---         <|> OpApprox <$ string "~"
---         <|> OpPlus   <$ string "+"
---         <|> OpMinus  <$ string "-"
---         <|> OpMult   <$ string "*"
-
--- monopExp = Monop <$> monop <*> expr
--- binopExp = Binop <$> expr <*> binop <*> expr
